6ff55adbed89f33091010acef2c1f1055b47073b
shibli <shiblisiddique1451@gmail.com>
2023-10-05 17:42:43 +0600

    react and bootstrap

---
+++ 01/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+
+
+  return (
+    <h2>Portfolio Project</h2>
+  );
+}
+
+export default App
